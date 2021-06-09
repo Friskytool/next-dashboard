7e65da8221965105bc5dad1220c4b6917148732e
Squidtoon99 <49101235+Squidtoon99@users.noreply.github.com>
2021-06-09 13:25:28 -0500

    removing prompt

--- pages/api/login.js
+++ pages/api/login.js
@@ -22,7 +22,7 @@ function handler(req, res) {
       process.env.CLIENT_ID
     }&redirect_uri=${encodeURIComponent(
       `${h}${req.headers.host}`
-    )}%2fapi%2fcallback&response_type=code&scope=guilds%20identify&prompt=none`;
+    )}%2fapi%2fcallback&response_type=code&scope=guilds%20identify`;
     console.log(uri);
     res.redirect(uri, 302);
   }
