133d7dbb96b79a60ae5f9d9c52b352d0d55d946e
Squidtoon99 <49101235+Squidtoon99@users.noreply.github.com>
2021-06-09 15:01:18 -0500

    returning user info instead

--- pages/api/login.js
+++ pages/api/login.js
@@ -4,12 +4,13 @@ import { withSession } from "next-session";
 function handler(req, res) {
   const data = req.session.userData;
   if (data) {
-    if (req.session.location) {
-      res.redirect(req.session.location, 301);
-      req.session.location = null;
-    } else {
-      res.redirect("/", 302);
-    }
+    // if (req.session.location) {
+    //   res.redirect(req.session.location, 301);
+    //   req.session.location = null;
+    // } else {
+    //   res.redirect("/", 302);
+    // }
+    res.status(200).json({ data: data });
   } else {
     let h;
     if (req.headers.host.startsWith("localhost:")) {
@@ -22,8 +23,7 @@ function handler(req, res) {
       process.env.CLIENT_ID
     }&redirect_uri=${encodeURIComponent(
       `${h}${req.headers.host}`
-    )}%2fapi%2fcallback&response_type=code&scope=guilds%20identify&prompt=consent`;
-    console.log(uri);
+    )}%2fapi%2fcallback&response_type=code&scope=guilds%20identify&prompt=none`;
     res.redirect(uri, 302);
   }
 }
